9d530a55ceb0bad28eec63ccb067212624476e1b
John Gully <john@gully.us>
2022-03-04 15:45:37 -0600

    Updated npm packages

--- mp3-download.mjs
+++ mp3-download.mjs
@@ -3,7 +3,7 @@ import crypto from "crypto";
 import fs from "fs";
 import path from "path";
 import { v4 as uuidv4 } from 'uuid';
-import { DOMParser } from "xmldom";
+import { DOMParser } from "@xmldom/xmldom";
 import xpath from "xpath";
 import Config from "./utils/config.mjs"
 const parser = new DOMParser();
